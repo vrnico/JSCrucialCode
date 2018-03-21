ec3a71e676bae128014e3078ad594858c1095f83
Michael Albers and Nico Daunt <nico@>
2018-03-21 13:42:39 -0700

    correctly call up to 100 results per page

--- src/CrucialCode.js
+++ src/CrucialCode.js
@@ -8,16 +8,17 @@ export class CrucialCode {
 
   searchLang(displayData){
     $.ajax({
-       url: `http://hn.algolia.com/api/v1/search_by_date?query=javascript`,
+       url: `http://hn.algolia.com/api/v1/search_by_date?query=javascript&hitsPerPage=100`,
        type: 'GET',
        data: {
          format: 'json'
        },
        success: function(response) {
          let test = [];
-         for (let i = 0; i < 10; i++){
-           if ((response.hits[i].story_url != null) && (response.hits[i].story_title != null)) {
+         for (let i = 0; i < 100; i++){
+           if ((response.hits[i].url != null) && (response.hits[i].title != null)) {
              test.push(response.hits[i]);
+
            }
          }
          displayData(test);
--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
   console.log("call happens");
   let newCrucialCode = new CrucialCode("javascript");
   let displayData = function updateResults(array) {
-    for (let i = 0; i < array.length; i++){
-      $('#searchDisplay').append('<li><h4><a href="' + array[i].story_url + '">' + array[i].story_title + '</a></h4></li>');
+    for (let i = 0; i < 10; i++){
+      $('#searchDisplay').append('<li><h4><a href="' + array[i].url + '">' + array[i].title + '</a></h4></li>');
     }
   }
   newCrucialCode.searchLang(displayData);
